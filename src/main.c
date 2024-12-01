3238856ee6e0729cabcaeee63377b380607badc1
Hugo Lindström <huggepugge1@gmail.com>
2024-12-01 13:07:51 +0100

    extracted destructors

--- src/main.c
+++ src/main.c
@@ -12,33 +12,36 @@ struct cell {
 };
 
 void test_array() {
-    const size_t ARRAY_SIZE = 10000;
-    int **array = allocate_array(ARRAY_SIZE, sizeof(int *), NULL, 1);
+    const size_t ARRAY_SIZE = 1000;
+    size_t **array = allocate_array(ARRAY_SIZE, sizeof(size_t *), NULL, 1);
     retain(array);
 
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        array[i] = allocate(sizeof(int), NULL, 0);
+    for (size_t i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = allocate(sizeof(size_t), NULL, 0);
         retain(array[i]);
         *array[i] = i;
     }
 
-    printf("%lu\n", sizeof(int) * ARRAY_SIZE + sizeof(int *) * ARRAY_SIZE);
+    printf("%lu\n",
+           sizeof(size_t) * ARRAY_SIZE + sizeof(size_t *) * ARRAY_SIZE);
     release(array);
 }
 
+void cell_destructor(obj *c) { release(((struct cell *)c)->cell); }
+
 int main(void) {
-    struct cell *c = allocate(sizeof(struct cell), NULL, 2);
+    struct cell *c = allocate(sizeof(struct cell), cell_destructor, 1);
     retain(c);
 
     retain(c->string);
 
-    c->cell = allocate(sizeof(struct cell), NULL, 1);
+    c->cell = allocate(sizeof(struct cell), cell_destructor, 1);
     c->cell->i = 42;
     retain(c->cell);
     c->cell = NULL;
 
     release(c->cell);
-    c->cell = allocate(sizeof(struct cell), NULL, 1);
+    c->cell = allocate(sizeof(struct cell), cell_destructor, 1);
     c->cell->i = 40;
     retain(c->cell);
 
